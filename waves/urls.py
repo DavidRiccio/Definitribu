6508c2086b942d42aac6413cd1b73ed80051d7d4
root <root@DESKTOP-T61IP4R>
2024-11-07 12:39:12 +0000

    Updated templates, styles, and views for echo and wave details, added hero section, and made responsive adjustments.

--- waves/urls.py
+++ waves/urls.py
@@ -1,4 +1,8 @@
+from django.urls import path
+from . import views
+
 app_name = 'waves'
 
-
-urlpatterns = []
+urlpatterns = [
+    path('<int:wave_pk>/', views.wave_detail, name='wave_detail'),  # Asegúrate de que el nombre del parámetro sea correcto
+]
--- waves/views.py
+++ waves/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .models import Wave
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.decorators import login_required
+from .models import Wave
+
+
+@login_required
+def wave_detail(request, wave_pk):
+    # Obtiene el objeto Wave o devuelve un error 404 si no se encuentra
+    wave = get_object_or_404(Wave, pk=wave_pk)
+    
+    # Renderiza la plantilla con el objeto wave
+    return render(request, 'waves/wave_detail.html', {'wave': wave})
